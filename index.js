77a9a83b1798de43f54573507d04316920792cdf
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-04-14 21:07:38 +0100

    Change port

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const server = require('./api/server');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => { console.log(`Listening on http://localhost: ${PORT}`)});
