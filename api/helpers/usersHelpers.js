8d7209bc5d371379b1e0f44f9bb585ebb172b0e7
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-04-15 13:08:36 +0100

    Create usersHelpers.js

---
+++ api/helpers/usersHelpers.js
@@ -0,0 +1,32 @@
+const db = require('../../data/dbConfig');
+
+const getUsers = () => {
+  return db('users');
+};
+
+const getUserById = (id) => {
+  return db('users')
+      .where({ id })
+      .first();
+};
+
+const addUser = async (user) => {
+  const [id] = await db('users').insert(user);
+  return db('users').where({ id }).first();
+};
+
+const deleteUser = (id) => {
+  return db('users').where({ id }).del();
+};
+
+const editUser = (user, id) => {
+  return db('users').where({ id }).update(user);
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  addUser,
+  deleteUser,
+  editUser,
+};
