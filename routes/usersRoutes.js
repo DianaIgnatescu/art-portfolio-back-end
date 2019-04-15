d7d70201a1ac2c5003a300ecd1278786db8fbf59
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-04-15 14:10:19 +0100

    Fix indentation

--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -18,16 +18,16 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   const { id } = req.params;
   Users.getUserById(id)
-      .then((user) => {
-        if(!user) {
-          res.status(404).json({ message: 'The user with the specified ID does not exist.'});
-        } else {
-          res.status(200).json(user);
-        }
-      })
-      .catch((error) => {
-        res.status(500).json({ error: 'The user information could not be retrieved.' });
-      });
+    .then((user) => {
+      if(!user) {
+        res.status(404).json({ message: 'The user with the specified ID does not exist.'});
+      } else {
+        res.status(200).json(user);
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'The user information could not be retrieved.' });
+    });
 });
 
 router.delete('/:id', (req, res) => {
@@ -53,16 +53,16 @@ router.put('/:id', (req, res) => {
     user.password = hash;
   }
   Users.editUser(user, id)
-      .then((data) => {
-        if(!data) {
-          res.status(404).json({ message: 'The user with the specified ID does not exist.' });
-        } else {
-          res.status(200).json({ user: { id, ...user }});
-        }
-      })
-      .catch((error) => {
-        res.status(500).json({ error: 'The user information could not be modified'});
-      });
+    .then((data) => {
+      if(!data) {
+        res.status(404).json({ message: 'The user with the specified ID does not exist.' });
+      } else {
+        res.status(200).json({ user: { id, ...user }});
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'The user information could not be modified'});
+    });
 });
 
 module.exports = router;
