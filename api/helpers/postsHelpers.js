eb667bb89d5592602d340f18f98d3e68393c4c4f
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-04-16 23:06:05 +0100

    Fix linting issues & refactor

--- api/helpers/postsHelpers.js
+++ api/helpers/postsHelpers.js
@@ -1,43 +1,30 @@
 const db = require('../../data/dbConfig');
 
-const getPosts = () => {
-  return db('posts');
-};
+const getPosts = () => db('posts');
 
-const getPostById = (id) => {
-  return db('posts')
-    .where({ id})
-    .first();
-};
+const getPostById = id => db('posts')
+  .where({ id })
+  .first();
 
-const getPostsFromUser = (id) => {
-  return db('posts')
-    .where({ userId: id })
-};
+const getPostsFromUser = id => db('posts').where({ userId: id });
 
-const addPost = (post) => {
-  return db('posts')
-    .insert(post)
-    .then(ids => {
-      return getPostById(ids[0]);
-    });
-};
+const addPost = post => db('posts')
+  .insert(post)
+  .then(ids => getPostById(ids[0]));
 
-const deletePost = (id) => {
-  return db('posts')
-    .where({id})
-    .del();
-};
+const deletePost = id => db('posts')
+  .where({ id })
+  .del();
 
-const editPost = (post, id) => {
-  return db('posts')
-    .where({ id })
-    .update(post);
-};
+const editPost = (post, id) => db('posts')
+  .where({ id })
+  .update(post);
+
+const getAllUpvotes = async () => db('likes');
 
 const getUpvotes = async (postId) => {
   const query = await db('likes').where({ postId }).count('id as CNT');
-  const total = query[0]['CNT'];
+  const total = query[0].CNT;
   return total;
 };
 
@@ -47,12 +34,12 @@ const upvote = async (userId, postId) => {
     .where({ userId })
     .count('id as CNT');
 
-  const total = query[0]['CNT'];
+  const total = query[0].CNT;
 
   if (total) {
     throw new Error('Like already in database');
   } else {
-    return await db('likes').insert({ userId, PostId: postId });
+    return await db('likes').insert({ userId, postId });
   }
 };
 
@@ -62,7 +49,7 @@ const downvote = async (userId, postId) => {
     .where({ userId })
     .count('id as CNT');
 
-  const total = query[0]['CNT'];
+  const total = query[0].CNT;
 
   if (!total) {
     throw new Error('Like not in database');
@@ -85,4 +72,5 @@ module.exports = {
   upvote,
   downvote,
   getUpvotes,
+  getAllUpvotes,
 };
