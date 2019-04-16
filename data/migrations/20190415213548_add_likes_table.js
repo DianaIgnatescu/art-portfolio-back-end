a751a9cd02ce42225d7e90ba036fe38d86dc6fe2
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-04-16 08:31:39 +0100

    Add table for liked posts

---
+++ data/migrations/20190415213548_add_likes_table.js
@@ -0,0 +1,24 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('likes', function(tbl) {
+    tbl.increments();
+    tbl
+        .integer('userId')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+    tbl
+        .integer('postId')
+        .unsigned()
+        .references('id')
+        .inTable('posts')
+        .onDelete('CASCADE')
+        .onUpdate('CASCADE');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('likes');
+};
