03a36f692f5b0d2a23c08b1bf5532788d8704423
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-04-16 23:08:34 +0100

    Refactor migrations

--- data/migrations/20190415104053_add_posts_table.js
+++ data/migrations/20190415104053_add_posts_table.js
@@ -1,27 +1,25 @@
 
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable('posts', function(tbl) {
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('posts', (tbl) => {
     tbl.increments();
     tbl
-        .string('postName', 255)
-        .notNullable();
+      .string('postName', 255)
+      .notNullable();
     tbl
-        .string('description', 255);
+      .string('description', 255);
     tbl
-        .string('imageUrl', 255)
-        .notNullable();
+      .string('imageUrl', 255)
+      .notNullable();
     tbl
-        .integer('upvotes');
-    tbl
-        .integer('userId')
-        .unsigned()
-        .references('id')
-        .inTable('users')
-        .onDelete('CASCADE')
-        .onUpdate('CASCADE');
+      .integer('userId')
+      .unsigned()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
   });
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function (knex, Promise) {
   return knex.schema.dropTableIfExists('posts');
 };
--- data/migrations/20190415213548_add_likes_table.js
+++ data/migrations/20190415213548_add_likes_table.js
@@ -1,24 +1,24 @@
 
-exports.up = function(knex, Promise) {
-  return knex.schema.createTable('likes', function(tbl) {
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('likes', (tbl) => {
     tbl.increments();
     tbl
-        .integer('userId')
-        .unsigned()
-        .references('id')
-        .inTable('users')
-        .onDelete('CASCADE')
-        .onUpdate('CASCADE');
+      .integer('userId')
+      .unsigned()
+      .references('id')
+      .inTable('users')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
     tbl
-        .integer('postId')
-        .unsigned()
-        .references('id')
-        .inTable('posts')
-        .onDelete('CASCADE')
-        .onUpdate('CASCADE');
+      .integer('postId')
+      .unsigned()
+      .references('id')
+      .inTable('posts')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
   });
 };
 
-exports.down = function(knex, Promise) {
+exports.down = function (knex, Promise) {
   return knex.schema.dropTableIfExists('likes');
 };
